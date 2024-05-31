9dd56a2ea7f284fb0c42b950ad7277a16e758ed2
katzeeee <104491909+katzeeee@users.noreply.github.com>
2024-05-31 11:20:22 +0900

    Create GMM.py

---
+++ GMM.py
@@ -0,0 +1,60 @@
+from sklearn.mixture import GaussianMixture
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def visualize_cluster_plot(clusterobj, dataframe, label_name, iscenter=True):
+    if iscenter :
+        centers = clusterobj.cluster_centers_
+
+    unique_labels = np.unique(dataframe[label_name].values)
+    markers=['o', 's', '^', 'x', '*']
+    isNoise=False
+
+    for label in unique_labels:
+        label_cluster = dataframe[dataframe[label_name]==label]
+        if label == -1:
+            cluster_legend = 'Noise'
+            isNoise=True
+        else :
+            cluster_legend = 'Cluster '+str(label)
+
+        plt.scatter(x=label_cluster['ftr1'], y=label_cluster['ftr2'], s=70,\
+                    edgecolor='k', marker=markers[label], label=cluster_legend)
+
+        if iscenter:
+            center_x_y = centers[label]
+            plt.scatter(x=center_x_y[0], y=center_x_y[1], s=250, color='white',
+                        alpha=0.9, edgecolor='k', marker=markers[label])
+            plt.scatter(x=center_x_y[0], y=center_x_y[1], s=70, color='k',\
+                        edgecolor='k', marker='$%d$' % label)
+    if isNoise:
+        legend_loc='upper center'
+    else: legend_loc='upper right'
+
+    plt.legend(loc=legend_loc)
+    plt.show()
+
+
+# make_blobs() 로 300개의 데이터 셋, 3개의 cluster 셋, cluster_std=0.5 을 만듬.
+X, y = make_blobs(n_samples=300, n_features=2, centers=3, cluster_std=0.5, random_state=0)
+
+# 길게 늘어난 타원형의 데이터 셋을 생성하기 위해 변환함.
+transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+X_aniso = np.dot(X, transformation)
+# feature 데이터 셋과 make_blobs( ) 의 y 결과 값을 DataFrame으로 저장
+clusterDF = pd.DataFrame(data=X_aniso, columns=['ftr1', 'ftr2'])
+clusterDF['target'] = y
+# 생성된 데이터 셋을 target 별로 다른 marker 로 표시하여 시각화 함.
+visualize_cluster_plot(None, clusterDF, 'target', iscenter=False)
+
+gmm = GaussianMixture(n_components=3, random_state=0)
+gmm_label = gmm.fit(X_aniso).predict(X_aniso)
+clusterDF['gmm_label'] = gmm_label
+
+# GaussianMixture는 cluster_centers_ 속성이 없으므로 iscenter를 False로 설정.
+visualize_cluster_plot(gmm, clusterDF, 'gmm_label',iscenter=False)
+
