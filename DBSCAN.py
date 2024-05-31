19427fbd2a9b36d0d5443251a378c6399bb2e845
katzeeee <104491909+katzeeee@users.noreply.github.com>
2024-05-31 11:17:02 +0900

    Create DBSCAN.py

---
+++ DBSCAN.py
@@ -0,0 +1,49 @@
+from sklearn.datasets import make_circles
+
+def visualize_cluster_plot(clusterobj, dataframe, label_name, iscenter=True):
+    if iscenter :
+        centers = clusterobj.cluster_centers_
+
+    unique_labels = np.unique(dataframe[label_name].values)
+    markers=['o', 's', '^', 'x', '*']
+    isNoise=False
+
+    for label in unique_labels:
+        label_cluster = dataframe[dataframe[label_name]==label]
+        if label == -1:
+            cluster_legend = 'Noise'
+            isNoise=True
+        else :
+            cluster_legend = 'Cluster '+str(label)
+
+        plt.scatter(x=label_cluster['ftr1'], y=label_cluster['ftr2'], s=70,\
+                    edgecolor='k', marker=markers[label], label=cluster_legend)
+
+        if iscenter:
+            center_x_y = centers[label]
+            plt.scatter(x=center_x_y[0], y=center_x_y[1], s=250, color='white',
+                        alpha=0.9, edgecolor='k', marker=markers[label])
+            plt.scatter(x=center_x_y[0], y=center_x_y[1], s=70, color='k',\
+                        edgecolor='k', marker='$%d$' % label)
+    if isNoise:
+        legend_loc='upper center'
+    else: legend_loc='upper right'
+
+    plt.legend(loc=legend_loc)
+    plt.show()
+
+
+X, y = make_circles(n_samples=1000, shuffle=True, noise=0.05, random_state=0, factor=0.5)
+clusterDF = pd.DataFrame(data=X, columns=['ftr1', 'ftr2'])
+clusterDF['target'] = y
+
+visualize_cluster_plot(None, clusterDF, 'target', iscenter=False)
+
+from sklearn.cluster import DBSCAN
+
+dbscan = DBSCAN(eps=0.2, min_samples=10, metric='euclidean')
+dbscan_labels = dbscan.fit_predict(X)
+clusterDF['dbscan_cluster'] = dbscan_labels
+
+visualize_cluster_plot(dbscan, clusterDF, 'dbscan_cluster', iscenter=False)
+
